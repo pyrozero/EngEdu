afb145408d2842e815371d84a492629f45097f37
Falish <pyro.zero@hotmail.com>
2017-10-19 20:40:53 +0700

    test01

---
+++ EngEdu/EngEdu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 255x semeter1,I am an Engedu freshman.");
+            Console.WriteLine("Year 255x semeter2,I Learn to program in C sharp.");
+                    }
+    }
+}
